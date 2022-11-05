9e947edadd9bf1bc289d65e39d170695f4b41e4a
yks72p <29257108+yks72p@users.noreply.github.com>
2022-11-05 16:25:02 +0300

    fixed circular imports in init.py and autograd.py

--- python/needle/autograd.py
+++ python/needle/autograd.py
@@ -1,9 +1,10 @@
 """Core data structures."""
 import needle
-from typing import List, Optional, NamedTuple, Tuple, Union
+from typing import List, Optional, NamedTuple, Tuple, Union, Dict
 from collections import namedtuple
 import numpy
-from needle import init
+
+from . import init
 
 # needle version
 LAZY_MODE = False
